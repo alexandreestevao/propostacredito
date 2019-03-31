ae7b73885283c65ca3e671356015a9947a71ee8e
Wesley F Pereira <jimmmisss@gmail.com>
2019-03-31 10:12:31 -0300

    Implementação de documentação swagger - implementação e uso.

--- src/main/java/br/com/calcard/controller/PropostaDeCreditoController.java
+++ src/main/java/br/com/calcard/controller/PropostaDeCreditoController.java
@@ -2,6 +2,7 @@ package br.com.calcard.controller;
 
 import br.com.calcard.persistence.model.PropostaDeCredito;
 import br.com.calcard.persistence.service.PropostaDeCreditoService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,16 +19,19 @@ public class PropostaDeCreditoController {
     @Autowired
     PropostaDeCreditoService propostaDeCreditoService;
 
+    @ApiOperation(value = "Retorna todas as análise de crédito.")
     @GetMapping
     public ResponseEntity<?> findAll() {
         return new ResponseEntity<>(propostaDeCreditoService.buscaTodos(), HttpStatus.OK);
     }
 
+    @ApiOperation(value="Retorna analise de credito por cpf.")
     @GetMapping(value = "/{cpf}")
     public ResponseEntity<?> findById(@PathVariable String cpf) {
         return new ResponseEntity<>(propostaDeCreditoService.buscarPorCpf(cpf), HttpStatus.OK);
     }
 
+    @ApiOperation(value="Registra analise de crédito no banco de dados.")
     @PostMapping
     public ResponseEntity<?> insert(@Valid @RequestBody PropostaDeCredito propostaDeCredito) {
         propostaDeCredito = this.propostaDeCreditoService.salvar(propostaDeCredito);
